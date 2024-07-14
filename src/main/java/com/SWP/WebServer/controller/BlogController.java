36be7491826d61fb9cd214d8048b83f5b132f502
ablue <tienvukk1@gmail.com>
2024-07-14 18:08:37 +0700

    blog

--- src/main/java/com/SWP/WebServer/controller/BlogController.java
+++ src/main/java/com/SWP/WebServer/controller/BlogController.java
@@ -1,11 +1,10 @@
 package com.SWP.WebServer.controller;
 
 import com.SWP.WebServer.entity.Blog;
+import com.SWP.WebServer.repository.BlogRepository;
 import com.SWP.WebServer.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +14,33 @@ public class BlogController {
 
     @Autowired
     private BlogService blogService;
+    @Autowired
+    private BlogRepository blogRepository;
 
     @GetMapping("/getAllBlogs")
     public List<Blog> getAllBlogs() {
         return blogService.getAllBlogs();
+    }
+    @GetMapping("/getAllBlogsIsActive")
+    public List<Blog> getAllActiveBlogs() {
+        return blogRepository.findByIsActiveTrue();
+    }
+    @GetMapping("/getAllActiveBlogsSortedByCreatedAt")
+    public List<Blog> getAllActiveBlogsSortedByCreatedAt() {
+        return blogService.getAllActiveBlogsSortedByCreatedAt();
+    }
+    @GetMapping("/recent")
+    public List<Blog> getRecentBlogs() {
+        return blogService.getRecentBlogs();
+    }
+
+    @GetMapping("/search")
+    public List<Blog> searchByTitle(@RequestParam String title) {
+        return blogService.searchByTitle(title);
+    }
+
+    @GetMapping("/{id}")
+    public Blog getBlogById(@PathVariable Long id) {
+        return blogService.getBlogById(id);
     }
 }
--- src/main/java/com/SWP/WebServer/service/BlogService.java
+++ src/main/java/com/SWP/WebServer/service/BlogService.java
@@ -5,6 +5,7 @@ import com.SWP.WebServer.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,8 +13,22 @@ public class BlogService {
 
     @Autowired
     private BlogRepository blogRepository;
-
     public List<Blog> getAllBlogs() {
         return blogRepository.findAll();
     }
+    public List<Blog> getAllActiveBlogsSortedByCreatedAt() {
+        return blogRepository.findByIsActiveTrueOrderByCreatedAtDesc();
+    }
+
+    public List<Blog> getRecentBlogs() {
+        return blogRepository.findTop5ByOrderByCreatedAtDesc();
+    }
+
+    public List<Blog> searchByTitle(String title) {
+        return blogRepository.findByTitleContainingIgnoreCase(title);
+    }
+    public Blog getBlogById(Long id) {
+        return blogRepository.findById(id).orElse(null);
+    }
+
 }
